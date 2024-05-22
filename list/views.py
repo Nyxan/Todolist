a93a9ba179c004f5fc73dd34ba944eb4f3014390
Oleksandr Moroz <molecxander@gmail.com>
2024-05-22 11:54:02 +0300

    add index page with list of tasks

--- list/views.py
+++ list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from list.models import Task
+
+
+def index(request):
+    tasks = Task.objects.all().order_by("is_done", "-created_at")
+    return render(request, "todolist/index.html", {"tasks": tasks})
